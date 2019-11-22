4271da31d470230a2b7ad61c10343b59c2252a79
fishu <pkaczoruk@wp.pl>
2019-11-22 16:25:07 +0100

    added 2 temporary header images, installed glide.js and modified header to contain the slider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import "@babel/polyfill"
 import './styles/style.scss'
+import Glide from '@glidejs/glide'
 
 import { validate } from './scripts/validate'
+
+new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  gap: 0,
+  autoplay: 5000,
+  hoverpause: false,
+  perTouch: 1,
+  animationDuration: 1000,
+  animationTimingFunc: 'ease-out'
+}).mount();
 
 window.validate = validate;
