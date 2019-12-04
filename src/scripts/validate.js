8109659c381d4342b4a693da9dceb84d0a099015
fishu <pkaczoruk@wp.pl>
2019-12-04 17:55:37 +0100

    added, styled and modified validation script for newsletter section

--- src/scripts/validate.js
+++ src/scripts/validate.js
@@ -9,22 +9,44 @@ export function validate() {
   // prevent default button behaviour
   event.preventDefault();
 
-  // get email, button and alert from form
-  const emailInput = document.getElementById('email-input');
-  const email = emailInput.value;
-  const btn = document.getElementById('sign-up');
+  // get name, email, button and alert from form
+  const emailInput = document.getElementById('email-input'),
+        email = emailInput.value,
+        nameInput = document.getElementById('name-input'),
+        name = nameInput.value.trim(),
+        btn = document.getElementById('sign-up');
+  let isEmailValid = false,
+      isNameValid = false;
 
-  // check if email is valid
+  // check if email is valid and add style
   if(validateEmail(email)) {
+    isEmailValid = true;
+    emailInput.style.border = '2px solid rgb(89, 206, 53)';
+  } else {
+    isEmailValid = false;
+    emailInput.style.border = '2px solid #F00';
+    btn.innerHTML = 'Try again';
+  }
+
+  // check if name is valid and add style
+  if(name != '') {
+    isNameValid = true;
+    nameInput.style.border = '2px solid rgb(89, 206, 53)';
+  } else {
+    isNameValid = false;
+    nameInput.style.border = '2px solid #F00';
+    btn.innerHTML = 'Try again';
+  }
+
+  // if everything is valid style and send to database
+  if(isNameValid && isEmailValid) {
+    nameInput.disabled = true;
+    emailInput.disabled = true;
     btn.innerHTML = 'Done!';
     btn.disabled = true;
     btn.style.cursor = 'default';
-    emailInput.style.padding = '8px 18px'
-    emailInput.style.border = '2px solid rgb(89, 206, 53)';
-  } else {
-    emailInput.style.padding = '8px 18px'
-    emailInput.style.border = '2px solid #F00';
-    btn.innerHTML = 'Try again';
+    // remove hover effect from button
+    btn.classList.remove('hover-effect')
   }
 
   return false;
